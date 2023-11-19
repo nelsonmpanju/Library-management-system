ff107272ad404e2b5d41a67afa969364f01b2818
nelsonmpanju <nelsonnorbert87@gmail.com>
2023-11-19 12:18:05 +0300

    Initial commit

---
+++ library_management/library_management/doctype/article/article.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Nelson Mpanju and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Article', {
+	// refresh: function(frm) {
+
+	// }
+});
